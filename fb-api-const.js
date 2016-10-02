80030551ca5a0e47499ed32b3273c612703797ab
Steve <steve@steve.com>
2016-10-02 15:17:16 -0700

    add api const module

---
+++ fb-api-const.js
@@ -0,0 +1,9 @@
+const fbAPIHostname      = 'api.fitbit.com';
+const fbAPIV1Endpoint    = '/1';
+const fbAPITokenEndpoint = '/oauth2/token';
+
+module.exports = {
+    fbAPIHostname,
+    fbAPIV1Endpoint,
+    fbAPITokenEndpoint
+}
